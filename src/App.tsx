7408c1f41cea05f722ed7eb5baf2fef05d813a11
ivandjoh <ivandj0h@users.noreply.github.com>
2024-12-08 13:17:30 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ButtonDemo } from "./components/custom-ui/button";
 import { Modal } from "./components/custom-ui/modal";
+import Navbar from "./components/navbar";
 
 const App: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -9,11 +10,13 @@ const App: React.FC = () => {
   const handleCloseModal = () => setModalOpen(false);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 space-y-4">
-      <h3 className="text-xl font-bold text-gray-800">Hello Vite!!</h3>
-      <ButtonDemo onClick={handleOpenModal} />
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 space-y-4">
+        <ButtonDemo onClick={handleOpenModal} />
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+      </div>
+    </>
   );
 };
 
--- src/components/custom-ui/modal.tsx
+++ src/components/custom-ui/modal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getPost, getUser } from "@/api/api";
 import { Post } from "@/interfaces/Post";
 import { User } from "@/interfaces/User";
+import { Loader } from "lucide-react";
 
 interface ModalProps {
   isOpen: boolean;
@@ -11,20 +12,27 @@ interface ModalProps {
 export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   const [post, setPost] = useState<Post | null>(null);
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const getRandomId = () => Math.floor(Math.random() * 100) + 1;
+  const IMAGE_URL = import.meta.env.VITE_IMAGE_URL;
 
   useEffect(() => {
     if (isOpen) {
+      setLoading(true);
+
       const fetchData = async () => {
         try {
           const randomId = getRandomId();
           const postData = await getPost(randomId);
           const userData = await getUser(postData.userId);
+
           setPost(postData);
           setUser(userData);
         } catch (error) {
           console.error("Error fetching data:", error);
+        } finally {
+          setLoading(false);
         }
       };
 
@@ -32,23 +40,31 @@ export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
     }
   }, [isOpen]);
 
-  if (!isOpen || !post || !user) return null;
+  if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg shadow-lg w-96">
-        <div className="p-4">
-          <img
-            src={`https://via.placeholder.com/150`}
-            alt="Placeholder"
-            className="rounded w-full h-48 object-cover"
-          />
-          <div className="mt-4">
-            <p className="text-sm text-gray-500">User: {user.name}</p>
-            <h3 className="text-xl font-bold">{post.title}</h3>
-            <p className="text-gray-700">{post.body}</p>
-            <p className="mt-2 font-semibold text-lg text-gray-800">$99</p>
-          </div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative bg-white rounded-lg shadow-lg w-96 z-50">
+        <div className="p-6">
+          {loading ? (
+            <div className="flex items-center justify-center h-48">
+              <Loader className="animate-spin text-blue-500 w-12 h-12" />
+            </div>
+          ) : (
+            <>
+              <img
+                src={IMAGE_URL}
+                alt="Placeholder"
+                className="rounded w-full h-48 object-cover"
+              />
+              <div className="mt-4">
+                <p className="text-sm text-gray-500">User: {user?.name}</p>
+                <h3 className="text-xl font-bold">{post?.title}</h3>
+                <p className="text-gray-700">{post?.body}</p>
+                <p className="mt-2 font-semibold text-lg text-gray-800">$99</p>
+              </div>
+            </>
+          )}
         </div>
         <button
           className="w-full py-2 bg-blue-500 text-white rounded-b hover:bg-blue-600"
